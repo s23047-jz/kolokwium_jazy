b696adc57a5f02fad5179783bdd53894560eb9fb
s23047-jz <s23047@pjwstk.edu.pl>
2022-07-03 17:03:05 +0200

    Added Rest Template Bean

--- src/main/java/pl/pjwstk/jazs23047nbp/JazS23047NbpApplication.java
+++ src/main/java/pl/pjwstk/jazs23047nbp/JazS23047NbpApplication.java
@@ -2,9 +2,14 @@ package pl.pjwstk.jazs23047nbp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class JazS23047NbpApplication {
+
+	@Bean
+	public RestTemplate restTemplate() {return new RestTemplate();}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JazS23047NbpApplication.class, args);
